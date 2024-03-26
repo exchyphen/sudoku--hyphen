68b9b7e868db0521a850c8dbb89dc93071eb9d46
exchyphen <exc.hyphen@gmail.com>
2024-03-25 19:14:10 -0700

    removed box, added focus, keypress events, value tracking

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import SudokuLibrary from "./SudokuLibrary.js";
 
-import Box from "./components/box";
+import Cell from "./components/cell.jsx";
 
 function App() {
-  const [focus, setFocus] = useState(undefined);
-
   const MAX_ROW = 9;
   const MAX_COL = 9;
 
-  // board data
+  const [focus, setFocus] = useState([-1, -1]);
+  const [valueArr, setValueArr] = useState(
+    Array.from(Array(MAX_ROW), () => Array(MAX_COL).fill(0))
+  );
 
+  // board data
   const data__board = Array.from(Array(MAX_ROW), () => Array(MAX_COL));
   // initialize data board
   for (let row = 0; row < MAX_ROW; row++) {
@@ -20,15 +22,54 @@ function App() {
         row: row,
         col: col,
         box: SudokuLibrary.findBox(row, col),
-        value: 0,
-        corner: [],
-        center: [],
         given: false,
       };
     }
   }
 
+  // helpful functions
+
+  // input: 2d array, output: deep copy of 2d array
+  const copyArr = (arr) => {
+    const newArr = Array.from(Array(arr.length), () =>
+      Array.from(arr[0].length)
+    );
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[0].length; j++) {
+        newArr[i][j] = arr[i][j];
+      }
+    }
+
+    return newArr;
+  };
+
   // handlers
+  const handleCellClick = (row, col) => {
+    console.log("handling cell click", row, col);
+
+    setFocus([row, col]);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode < 49 || e.keyCode > 57) {
+      return;
+    }
+
+    console.log("key", e.keyCode);
+
+    // if no focus, do nothing
+    if (focus[0] >= 0 && focus[1] >= 0) {
+      console.log("writing to", focus[0], focus[1]);
+      console.log("value", e.keyCode - 49 + 1);
+
+      const newArr = copyArr(valueArr);
+
+      newArr[focus[0]][focus[1]] = e.keyCode - 49 + 1;
+
+      setValueArr(newArr);
+    }
+  };
 
   // helper function: create the board from cell components
   const createBoard = () => {
@@ -39,18 +80,32 @@ function App() {
 
     for (let row = 0; row < MAX_ROW; row++) {
       for (let col = 0; col < MAX_COL; col++) {
-        boxArr[data__board[row][col].box].push(data__board[row][col]);
+        const data = data__board[row][col];
+        boxArr[data.box].push(
+          <Cell
+            key={`cell_R${data.row}_C${data.col}`}
+            data={data}
+            value={valueArr[data.row][data.col]}
+            corner={[]}
+            center={[]}
+            onCellClick={handleCellClick}
+            focus={focus[0] === data.row && focus[1] === data.col}
+          ></Cell>
+        );
       }
     }
 
-    return boxArr.map((data, index) => {
-      return <Box key={`box${index}`} data={data}></Box>;
+    return boxArr.map((boxData, index) => {
+      return (
+        <section key={`box${index}`} className="box">
+          {boxData}
+        </section>
+      );
     });
   };
 
   useEffect(() => {
-    // test js
-    console.log(data__board);
+    // fetch data
   }, []);
 
   return (
@@ -61,10 +116,10 @@ function App() {
           <p>A sudoku tool.</p>
         </hgroup>
       </header>
-      <main className="main">
-        <p>this is the board</p>
-
-        <article className="board">{createBoard()}</article>
+      <main className="main prevent-select">
+        <article className="board" tabIndex="0" onKeyDown={handleKeyDown}>
+          {createBoard()}
+        </article>
       </main>
       <footer>
         <hgroup>
--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -3,7 +3,32 @@ import "./cell.css";
 const Cell = (props) => {
   const data = props.data;
 
-  return <div className="cell">{data.value}</div>;
+  // handlers
+  const handleClick = () => {
+    console.log("focus on", data.row, data.col);
+
+    props.onCellClick(data.row, data.col);
+  };
+
+  const handleMouseEnter = (e) => {
+    e.preventDefault();
+
+    if (e.buttons === 1) {
+      console.log("mouse entered cell", data.row, data.col);
+    }
+  };
+
+  return (
+    <div
+      className={"cell" + (props.focus ? " cell--focus" : "")}
+      // onMouseEnter={handleMouseEnter}
+      onMouseDown={handleClick}
+    >
+      {props.value === 0 ? null : (
+        <div className="cell--solved">{props.value}</div>
+      )}
+    </div>
+  );
 };
 
 export default Cell;
